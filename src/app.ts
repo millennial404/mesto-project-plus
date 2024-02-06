a68835f77431cff0bb24f11a2cdc9e6724f22dfb
Sergei Zakharchenko <millennial404@yandex.ru>
2024-02-06 22:40:14 +0300

    feat: models

---
+++ src/app.ts
@@ -0,0 +1,10 @@
+import express from 'express';
+import mongoose from 'mongoose';
+
+const { PORT = 3000 } = process.env;
+
+const app = express();
+mongoose.connect('mongodb://localhost:27017/mestodb');
+app.listen(PORT, () => {
+  console.log(`App listening on port ${PORT}`)
+})
